e500874e97c0248908d5ddd5a8e399dc1ecff3b5
vinit sharma <vinitsharma08210821@gmail.com>
2025-07-15 15:30:48 +0530

    first commit

--- index9.py
+++ index9.py
@@ -1,5 +1,5 @@
-[4:25 pm, 11/07/2025] Vinit_0821:     i+=1
-[4:25 pm, 11/07/2025] Vinit_0821: # for i in range(1,5):
+[4:25 pm, 11/7/2025] Vinit_0821:     i+=1
+[4:25 pm, 11/7/2025] Vinit_0821: # for i in range(1,5):
 #     for j in range(10,16):
 #         print("*", end=" ")
 #     print("")
@@ -10,7 +10,7 @@
 #     print("")
 
 n = int(input("Enter the number of rows you want: "))
-
+                                                                     
 for i in range(1,n+1):
     for j in range(1, i+1):
         print("*", end=" ")
